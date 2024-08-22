b6e9bf3ee2428dab5da4f1d92f31f4a6ba3418a8
Mahesh <girimahesh.m@gmail.com>
2024-08-22 11:57:03 +0530

    Complete Auth Page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import { Button } from "./components/ui/button";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Profile from "./pages/Profile";
+import Auth from "./pages/Auth";
+import Chat from "./pages/Chat";
 
 function App() {
-    return <Button>Click Me</Button>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="*" element={<Navigate to="/auth" />} />
+                
+                <Route path="/auth" element={<Auth />} />
+                <Route path="/chat" element={<Chat />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
